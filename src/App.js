814e1085daf6bfd99c543cb06874ddb4bbb444d6
Justice Isreal <justiceyba@gmail.com>
2022-09-05 23:45:58 +0100

    form structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="iv-card">
+        <div className="pick-table">
+          <button>RESERVE A SEAT</button>
+        </div>
+        <div className="form">
+          <form>
+            <h3>This invitation only covers for a MAXIMUM 0F 2 PERSONS</h3>
+            <div>
+              <label>Name :</label>
+              <input type="text" placeholder="Enter Full Name" />
+            </div>
+            <div>
+              <label>Phone No.:</label>
+              <input type="number" placeholder="Enter Phone Number" />
+            </div>
+            <div>
+              <label>You will attend ? </label>
+              <select>
+                <option value="" key="">
+                  Select
+                </option>
+                <option value="Tradition Marriage" key="">
+                  Tradition Marriage
+                </option>
+                <option value="White Wedding" key="">
+                  White Wedding
+                </option>
+                <option value="Both" key="">
+                  Both
+                </option>
+              </select>
+            </div>
+            <div>
+              <label>
+                Will you be coming with a Spouse? If enter Spouse Name else
+                leave blank
+              </label>
+              <input type="text" placeholder="Enter Spouse Name" />
+            </div>
+
+            <button type="submit">SUBMIT</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
