e2b1c94ad7245100b838b5fe219aaeab1feada33
Justice Isreal <justiceyba@gmail.com>
2022-09-29 07:46:17 +0100

    table page set up

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ function App() {
           ></Route>
           <Route
             path="/UsersTable"
-            element={<UsersTable users={people} />}
+            element={<UsersTable Data={people} />}
           ></Route>
         </Routes>
         {/* <UsersTable users={people} /> */}
