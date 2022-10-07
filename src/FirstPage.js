4f1aa5e47f2d4154b7ce993ff8d5afd5bd08caf5
Justice Isreal <justiceyba@gmail.com>
2022-10-07 21:32:14 +0100

    download prewedding photos

--- src/FirstPage.js
+++ src/FirstPage.js
@@ -4,151 +4,142 @@ import { Fade } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
 
 function FirstPage() {
-  const fadeImages = [
-    {
-      id: 21,
-      img: "https://res.cloudinary.com/isreal/image/upload/v1665121674/IMG_7669_be06x4.jpg",
-      download:
-        "https://res.cloudinary.com/isreal/image/upload/fl_attachment:my_custom_filename/IMG_7669_be06x4.jpg",
-    },
+  const fadeImage = [
     {
       id: 1,
-      img: "https://res.cloudinary.com/isreal/image/upload/v1665121443/prewed8_tygopq.jpg",
+      img: "https://res.cloudinary.com/isreal/image/upload/v1665146667/IMG_4639_aalrms.jpg",
       download:
-        "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/prewed8_tygopq.jpg",
+        "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/IMG_4639_aalrms.jpg",
     },
     {
       id: 2,
-      img: "https://res.cloudinary.com/isreal/image/upload/v1665121598/prewed9_tgsaig.jpg",
-      download:
-        "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/prewed9_tgsaig.jpg",
-    },
-    {
-      id: 22,
-      img: "https://res.cloudinary.com/isreal/image/upload/v1665146673/IMG_4643_s4szdp.heic",
-      download:
-        "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/IMG_7749_focd5j.jpg",
-    },
-    {
-      id: 3,
-      img: "https://res.cloudinary.com/isreal/image/upload/v1665121720/IMG_7749_focd5j.jpg",
-      download:
-        "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/IMG_7749_focd5j.jpg",
-    },
-    {
-      id: 4,
-      img: "https://res.cloudinary.com/isreal/image/upload/v1665121515/prewed2_aqum47.jpg",
-      download:
-        "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/prewed2_aqum47.jpg",
-    },
-    {
-      id: 5,
-      img: "https://res.cloudinary.com/isreal/image/upload/v1665121519/prewed3_zursg2.jpg",
-      download:
-        "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/prewed3_zursg2.jpg",
-    },
-    {
-      id: 6,
       img: "https://res.cloudinary.com/isreal/image/upload/v1665121726/IMG_7758_qkzyho.jpg",
       download:
         "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/IMG_7758_qkzyho.jpg",
     },
     {
-      id: 7,
-      img: "https://res.cloudinary.com/isreal/image/upload/v1665121593/prewed6_rnuplp.jpg",
+      id: 3,
+      img: "https://res.cloudinary.com/isreal/image/upload/v1665146585/IMG_4593_qmo1kq.jpg",
       download:
-        "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/prewed6_rnuplp.jpg",
+        "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/IMG_4593_qmo1kq.jpg",
     },
     {
-      id: 70,
-      img: "https://res.cloudinary.com/isreal/image/upload/v1665146622/IMG_4635_aiqqnk.heic",
+      id: 4,
+      img: "https://res.cloudinary.com/isreal/image/upload/v1665146670/IMG_4642_u8tuwp.jpg",
       download:
-        "https://res.cloudinary.com/isreal/image/upload/v1665146622/IMG_4635_aiqqnk.heic",
+        "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/IMG_4642_u8tuwp.jpg",
     },
     {
-      id: 8,
-      img: "https://res.cloudinary.com/isreal/image/upload/v1665121520/prewed4_hgns8t.jpg",
-      download:
-        "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/prewed4_hgns8t.jpg",
-    },
-    {
-      id: 9,
-      img: "https://res.cloudinary.com/isreal/image/upload/v1665121700/IMG_7686_fd67dt.jpg",
-      download:
-        "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/IMG_7686_fd67dt.jpg",
-    },
-    {
-      id: 10,
-      img: "https://res.cloudinary.com/isreal/image/upload/v1665121606/prewed12_vbilm0.jpg",
-      download:
-        "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/prewed12_vbilm0.jpg",
-    },
-    {
-      id: 11,
+      id: 5,
       img: "https://res.cloudinary.com/isreal/image/upload/v1665121660/IMG_7652_fgpa5p.jpg",
       download:
         "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/IMG_7652_fgpa5p.jpg",
     },
     {
-      id: 12,
-      img: "https://res.cloudinary.com/isreal/image/upload/v1665121523/prewed5_ggv71u.jpg",
+      id: 6,
+      img: "https://res.cloudinary.com/isreal/image/upload/v1665146605/IMG_4623_se44fk.jpg",
       download:
-        "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/prewed5_ggv71u.jpg",
+        "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/IMG_4623_se44fk.jpg",
     },
     {
-      id: 13,
+      id: 7,
+      img: "https://res.cloudinary.com/isreal/image/upload/v1665146673/IMG_4643_s4szdp.jpg",
+      download:
+        "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/IMG_4643_s4szdp.jpg",
+    },
+    {
+      id: 8,
+      img: "https://res.cloudinary.com/isreal/image/upload/v1665121674/IMG_7669_be06x4.jpg",
+      download:
+        "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/IMG_7669_be06x4.jpg",
+    },
+    {
+      id: 9,
+      img: "https://res.cloudinary.com/isreal/image/upload/v1665146598/IMG_4595_xsskjg.jpg",
+      download:
+        "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/IMG_4595_xsskjg.jpg",
+    },
+
+    {
+      id: 10,
+      img: "https://res.cloudinary.com/isreal/image/upload/v1665146668/IMG_4640_nuzqrp.jpg",
+      download:
+        "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/IMG_4640_nuzqrp.jpg",
+    },
+    {
+      id: 11,
       img: "https://res.cloudinary.com/isreal/image/upload/v1665121649/IMG_7664_xri1g4.jpg",
       download:
         "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/IMG_7664_xri1g4.jpg",
     },
     {
-      id: 14,
-      img: "https://res.cloudinary.com/isreal/image/upload/v1665121660/IMG_7652_fgpa5p.jpg",
+      id: 12,
+      img: "https://res.cloudinary.com/isreal/image/upload/v1665146661/IMG_4637_q9ohtd.jpg",
       download:
-        "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/IMG_7652_fgpa5p.jpg",
+        "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/IMG_4637_q9ohtd.jpg",
+    },
+    {
+      id: 13,
+      img: "https://res.cloudinary.com/isreal/image/upload/v1665146606/IMG_4624_fjqjuy.jpg",
+      download:
+        "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/IMG_4624_fjqjuy.jpg",
+    },
+    {
+      id: 14,
+      img: "https://res.cloudinary.com/isreal/image/upload/v1665121624/IMG_7590_1_hofn8v.jpg",
+      download:
+        "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/IMG_7590_1_hofn8v.jpg",
     },
     {
       id: 15,
-      img: "https://res.cloudinary.com/isreal/image/upload/v1665121601/prewed10_nv92oi.jpg",
+      img: "https://res.cloudinary.com/isreal/image/upload/v1665146653/IMG_4636_nqytv8.jpg",
       download:
-        "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/prewed10_nv92oi.jpg",
+        "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/IMG_4636_nqytv8.jpg",
     },
     {
       id: 16,
-      img: "https://res.cloudinary.com/isreal/image/upload/v1665121523/prewed5_ggv71u.jpg",
+      img: "https://res.cloudinary.com/isreal/image/upload/v1665146622/IMG_4635_aiqqnk.jpg",
       download:
-        "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/prewed5_ggv71u.jpg",
+        "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/IMG_4635_aiqqnk.jpg",
     },
     {
       id: 17,
-      img: "https://res.cloudinary.com/isreal/image/upload/v1665121624/IMG_7590_1_hofn8v.jpg",
-      download:
-        "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/IMG_7590_1_hofn8v.jpg",
-    },
-    {
-      id: 18,
-      img: "https://res.cloudinary.com/isreal/image/upload/v1665121624/IMG_7590_1_hofn8v.jpg",
-      download:
-        "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/IMG_7590_1_hofn8v.jpg",
-    },
-    {
-      id: 19,
       img: "https://res.cloudinary.com/isreal/image/upload/v1665121639/IMG_7646_in0o4x.jpg",
       download:
         "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/IMG_7646_in0o4x.jpg",
     },
     {
-      id: 20,
-      img: "https://res.cloudinary.com/isreal/image/upload/v1665121649/IMG_7664_xri1g4.jpg",
+      id: 18,
+      img: "https://res.cloudinary.com/isreal/image/upload/v1665146593/IMG_4594_nuyehh.jpg",
       download:
-        "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/IMG_7664_xri1g4.jpg",
+        "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/IMG_4594_nuyehh.jpg",
+    },
+    {
+      id: 19,
+      img: "https://res.cloudinary.com/isreal/image/upload/v1665121683/IMG_7686_1_tq8mav.jpg",
+      download:
+        "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/IMG_7686_1_tq8mav.jpg",
+    },
+    {
+      id: 20,
+      img: "https://res.cloudinary.com/isreal/image/upload/v1665121720/IMG_7749_focd5j.jpg",
+      download:
+        "https://res.cloudinary.com/isreal/image/upload/fl_attachment:JJ2022/IMG_7749_focd5j.jpg",
     },
   ];
 
   return (
-    <div>
+    <div
+      style={{
+        border: "5px solid goldenrod",
+        padding: "5px",
+        borderRadius: "10px",
+        // width: "100vw",
+        overflow: "hidden",
+      }}
+    >
       <Fade>
-        {fadeImages.map((fadeImage, index) => (
+        {fadeImage.map((fadeImage, index) => (
           <ImgDisplay key={index} {...fadeImage} />
         ))}
       </Fade>
@@ -161,12 +152,12 @@ function FirstPage() {
           //   justifyContent: "center",
           //   alignItems: "center",
           overflowX: "scroll",
-          //   border: "2px solid green",
+          // border: "2px solid green",
           margin: "0 auto",
           padding: "0 5px",
         }}
       >
-        {fadeImages.map((fadeImage, index) => (
+        {fadeImage.map((fadeImage, index) => (
           <ImgDownload key={index} {...fadeImage} />
         ))}
       </div>
@@ -194,7 +185,6 @@ function FirstPage() {
 function ImgDisplay({ img }) {
   return (
     <>
-      {" "}
       <div className="each-fade">
         <div
           className="image-container"
@@ -202,6 +192,9 @@ function ImgDisplay({ img }) {
             width: "330px",
             margin: "0 auto",
             backgroundColor: "whitesmoke",
+            // border: "1px solid red",
+            height: "410px",
+            borderRadius: "5px",
           }}
         >
           <img src={img} style={{ width: "100%", borderRadius: "3px" }} />
